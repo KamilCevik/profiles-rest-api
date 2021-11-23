a8c266d5b5ff3a2d8e7f1c6b77de8c77a953a4e5
Kamil Cevik <kamilahmetcevik@gmail.com>
2021-11-23 20:16:57 +0300

    Added improved permissions to use profile feed items API.

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -18,13 +18,22 @@ class UserProfilSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Create and return a new user"""
-        
-        user=models.UserProfile(
+
+        user = models.UserProfile(
             email=validated_data["email"],
             name=validated_data["name"]
         )
-        
+
         user.set_password(validated_data["password"])
         user.save()
-        
+
         return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """A serializer for profile feed items."""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ("id", "user_profile", "status_text", "created_on")
+        extra_kwargs = {"user_profile": {"read_only": True}}
